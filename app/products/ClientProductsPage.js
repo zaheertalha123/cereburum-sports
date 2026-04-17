b28402780683647f9bf536c352d966481fa297db
Muhammad Talha <zaheertalha123@gmail.com>
2026-04-17 16:02:27 +0500

    3 products added

--- app/products/ClientProductsPage.js
+++ app/products/ClientProductsPage.js
@@ -7,7 +7,7 @@ import styles from './products.module.css';
 
 export default function ClientProductsPage({ categories }) {
   // Adding synthetic filter options based on product types
-  const filterOptions = ['All', 'Nets', 'Hardware', 'Custom'];
+  const filterOptions = ['All', 'Cricket', 'Nets', 'Hardware', 'Custom'];
   const [activeFilter, setActiveFilter] = useState('All');
 
   const filteredCategories = activeFilter === 'All'
